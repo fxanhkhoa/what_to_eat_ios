f927ca53f57bccc80b926f83543f9ccdd9d4fb3f
fxanhkhoa <fxanhkhoa@gmail.com>
2025-08-02 13:32:43 +0700

    Initial Commit

---
+++ what-to-eat-ios/what_to_eat_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  what_to_eat_iosApp.swift
+//  what-to-eat-ios
+//
+//  Created by Khoa Bui on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct what_to_eat_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ what-to-eat-iosTests/what_to_eat_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  what_to_eat_iosTests.swift
+//  what-to-eat-iosTests
+//
+//  Created by Khoa Bui on 2/8/25.
+//
+
+import Testing
+@testable import what_to_eat_ios
+
+struct what_to_eat_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
